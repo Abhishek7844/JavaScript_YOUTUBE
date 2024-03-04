f9aa7b0b22772a95e083cc59df6bcbd73cb24dd1
Abhishek7844 <107628951+Abhishek7844@users.noreply.github.com>
2024-03-04 16:25:47 +0000

    container config done

---
+++ .devcontainer/Coda/print_inTABLE.js
@@ -0,0 +1,13 @@
+const rollNo = 1234
+
+var Name = "James"
+
+let password = 12345
+
+cityUser = "Chennai"
+
+Name = "Abhishek"
+
+let State;
+
+console.table([rollNo, Name, State, password, cityUser])
---
+++ .devcontainer/Coda/variable.js
@@ -0,0 +1,29 @@
+const rollNo = 1234
+// it is immutable it declared once only and not changed again
+
+var Name = "James"
+/*
+plz do not use var var has scope 
+issue and it allows 2 different var 
+with same name
+*/
+
+let password = 12345
+// it's best to declare any varibale
+
+cityUser = "Chennai"
+/*
+we can also declare any var like this 
+but it 's not good enough
+ */
+
+
+let State;  //here if we don't assign value to a var it will print Undefined
+
+Name = "Abhishek"
+
+console.log(rollNo)
+console.log(Name)
+console.log(cityUser)
+console.log(password)
+console.log(State)
