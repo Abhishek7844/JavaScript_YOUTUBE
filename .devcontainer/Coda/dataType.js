066f3c968126aed0304cc3c307a347c2c9d3fb46
Abhishek7844 <107628951+Abhishek7844@users.noreply.github.com>
2024-03-05 05:12:20 +0000

    container config done

---
+++ .devcontainer/Coda/dataType.js
@@ -0,0 +1,37 @@
+"use strict" 
+/*
+this is bring make old or ANCIENT code
+ in modern code or treat as modern code
+ one we write "use strict"
+ we can't go back nowww we have to write 
+ according to new version
+ */
+
+
+let age = 20
+let name = "Adam" 
+let address;
+
+console.log(3+3)
+
+console.log("Abhishek")
+
+//DATA types in JS
+/*
+number
+bigint
+string ""
+boolean True/False
+symbol => unique 
+null--> is a value itself and also a type
+undefinded => mean a var is created but never initialized
+
+
+object => most important dataType
+
+*/
+
+console.log()
+console.log(typeof age)
+console.log(typeof name)
+console.log(typeof address)
