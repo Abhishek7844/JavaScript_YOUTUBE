822eda1f414079b0702d7baaf21be0f4ee83ec46
Abhishek7844 <107628951+Abhishek7844@users.noreply.github.com>
2024-03-23 14:53:42 +0000

    update

--- .devcontainer/Coda/create_variable.js
+++ .devcontainer/Coda/create_variable.js
@@ -10,7 +10,8 @@
 //create variable is same like buying a plot and constructing anytype of building;
 // similarly by var we hold(buying) a space in memory and store(constructing) any kind of value
 
-// using *****let*****, in let we can not create 2+ var with same name
+// using *****let*****, in let we can not create 2+ var with same name,  
+//can NOT be accessed out of scope
 
 
 let username = 'Mark Tyson';
@@ -21,7 +22,8 @@ console.log(username);
 
 
 
-//using -----var----, it allows we can create 2+ var with same name
+//using -----var----, it allows we can create 2+ var with same name,
+// can be accessed out of scope
 
 var username1 = "John Cina";
 console.log(username1);
@@ -30,10 +32,19 @@ var username1 = "Randy Orton";
 console.log(username1);
 
 //using -----const----, it allows we can create N assign value only once, we can never change it's value gain
-
+//can NOT be accessed out of scope
 
 const pi = 3.14;
 console.log(pi);
 
-//pi=3.15;                                       it will not executed and show error 
+//pi=3.15;                                       it will not be executed and show error 
 //console.log(pi);
+
+
+//---------------------------------------------------------------------------------------------------------------//
+
+let firstname = "Nikki";
+let lastName = "Bella";
+let age = 39;
+
+console.log("Namaste I am "+ firstname+" "+ lastName+" & i'm "+age+" year old.");
