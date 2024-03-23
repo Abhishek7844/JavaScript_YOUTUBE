baf68263ea23bad1a47caa590d976a641080584b
Abhishek7844 <107628951+Abhishek7844@users.noreply.github.com>
2024-03-23 14:07:16 +0000

    createVaribale.js

---
+++ .devcontainer/Coda/create_variable.js
@@ -0,0 +1,39 @@
+// We can create variable in js by 3-ways:------------------------>
+
+/*
+1. let ---------->>     (bestammmm best)
+2. var ---------->>     (plz try to avoid)
+3. const -------->>     (for constant values(unchaged))
+*/
+
+
+//create variable is same like buying a plot and constructing anytype of building;
+// similarly by var we hold(buying) a space in memory and store(constructing) any kind of value
+
+// using *****let*****, in let we can not create 2+ var with same name
+
+
+let username = 'Mark Tyson';
+console.log(username);
+
+username = 'Mike Tyson is correct name '
+console.log(username);
+
+
+
+//using -----var----, it allows we can create 2+ var with same name
+
+var username1 = "John Cina";
+console.log(username1);
+
+var username1 = "Randy Orton";
+console.log(username1);
+
+//using -----const----, it allows we can create N assign value only once, we can never change it's value gain
+
+
+const pi = 3.14;
+console.log(pi);
+
+//pi=3.15;                                       it will not executed and show error 
+//console.log(pi);
